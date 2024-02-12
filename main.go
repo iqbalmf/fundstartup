50fa45f5a2d01d4b7d2e1c160a68ac4706b13ae9
Iqbal Fauzan <iqbalmf68@gmail.com>
2024-02-12 08:49:32 +0700

    adding api for save input users

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"funding-app/handler"
+	"funding-app/users"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	dsn := "root:password@tcp(127.0.0.1:3306)/fundstartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	fmt.Println("Connection to Database going well")
+	userRepository := users.NewRepository(db)
+	userService := users.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+	api.POST("/users", userHandler.RegisterUser)
+	router.Run()
+
+	//input dari user
+	//handler, mapping input dari user menjadi sebuah struct input
+	//service, melakukan mapping dari struct input ke struct User
+	//repository,
+	//db
 }
